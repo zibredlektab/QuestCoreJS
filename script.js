3af9591fd0075694a004fd102cbaa7444c8c13ee
Max <max@maxbatchelder.com>
2016-11-08 13:46:40 -0500

    forward click box now appears for nodes where it is appropriate. also cleaned up the xml parsing so it only looks at views for the current room

--- script.js
+++ script.js
@@ -1,12 +1,10 @@
-//$.getScript("room1.js"); //note that this only works over http://, not file:// (for local testing)
-
-
 var $area, $currentroom, $currentview;
 
 var viewindex = 0;
 
 $(document).ready(function(){
 
+	// load the starting area
 	loadArea("room1");
 
 	// left nav button logic
@@ -21,6 +19,10 @@ $(document).ready(function(){
 
 });
 
+
+
+// loadArea loads a set of area data (various rooms within an area, their views, etc) from
+// an XML file named [areaName].xml. It then navigates to index 0 in that room (for now)
 function loadArea(areaName) {
 	$.ajax({
 		type: "GET",
@@ -28,7 +30,24 @@ function loadArea(areaName) {
 		dataType: "xml",
 		success: function(xml) {
 			$area = $(xml);
-			console.log("area "+ areaName + " loaded!")
+
+			// assign $currentroom to the room matching the requested ID (encapsulate this eventually)
+			var foundcurrentroom = false;
+			$area.find("room").each(function(){
+				if ($(this).attr("ID") == "kitchen" && !foundcurrentroom) {
+					$currentroom = $(this);
+					foundcurrentroom = true;
+				}
+			});
+
+			// make sure we actually found the requested room
+			if (!foundcurrentroom) {
+				alert("requested room doesn't exist in this area");
+			}
+
+			//console.log("area "+ areaName + " loaded");
+			//console.log("currently in room " + $currentroom.attr("ID"));
+
 			navToIndex(0);
 		}
 	});
@@ -36,19 +55,19 @@ function loadArea(areaName) {
 
 
 
-// turnTo is a macro to simplify the logic of cycling from view to view within a room, and
-// then navigating to the appropriate view.
+// turnTo simplifies the logic of cycling from view to view within a room (without overflowing
+// the bounds of the room, and then navigating to the appropriate view.
 function turnTo(direction) {
 	if (direction == undefined) {
 		direction = "left";
 	} else if (direction == "left") {
 		viewindex--;
 		if (viewindex < 0) {
-			viewindex = 3;
+			viewindex = $currentroom.find("view").length-1;
 		}
 	} else if (direction == "right") {
 		viewindex++;
-		if (viewindex >= $area.find("view").length - 1) {
+		if (viewindex >= $currentroom.find("view").length) {
 			viewindex = 0;
 		}
 	}
@@ -60,9 +79,39 @@ function turnTo(direction) {
 // navToIndex does the brunt work of swapping the background image, based on the provided
 // "index" argument.
 function navToIndex(index) {
+	// I don't know in what circumstance I'd ever call this function without a valid index, but just in case...
 	if (index == undefined) {
+		console.log("index to navigate to was undefined, navigating to 0");
 		index = 0;
 	}
-	$(".game").css("background-image", "url(\"img/kitchen-" + $area.find("view").eq(index).text() + ".png\")");
-	//$(".game").css("background-image" , "url(\"img/" + kitchenviews[index] + ".png\")");
+
+	console.log ("navigating to index " + index);
+
+	$currentview = $area.find("view").eq(index);
+	$(".game").css("background-image", "url(\"img/" + $currentroom.attr("ID") + "-" + $currentview.attr("ID") + ".png\")");
+
+	// determine whether the "forward" clickbox should be active & make it so (this does not belong in this function)
+	if ($currentview.has("fwd-room").length > 0) {
+		//console.log("forward exists here");
+		$("#forward").css("display", "block");
+	} else {
+		//console.log("forward does not exist here");
+		$("#forward").css("display", "none");
+	}
 }
+
+
+
+
+
+
+/* USEFUL DEBUG THINGS
+
+
+
+	// print out all of the views associated with this room (and only this room)
+	$currentroom.find("view").each(function(){
+		console.log($(this).attr("ID"));
+	});
+
+*/
