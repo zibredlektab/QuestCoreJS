79196281021676f8380cfa71d9718ee2506a5e84
Max <max@maxbatchelder.com>
2016-11-08 12:32:06 -0500

    Moved area info to xml file and began work on xml parsing

--- script.js
+++ script.js
@@ -1,20 +1,39 @@
-$.getScript("room1.js"); //note that this only works over http://, not file:// (for local testing)
+//$.getScript("room1.js"); //note that this only works over http://, not file:// (for local testing)
+
+
+var $area, $currentroom, $currentview;
 
 var viewindex = 0;
 
 $(document).ready(function(){
 
+	loadArea("room1");
+
 	// left nav button logic
 	$("#left").click(function(){
 		turnTo("left");
 	});
-	
+
 	// right nav button logic
 	$("#right").click(function(){
 		turnTo("right");
 	});
-	
+
 });
+
+function loadArea(areaName) {
+	$.ajax({
+		type: "GET",
+		url: areaName + ".xml",
+		dataType: "xml",
+		async: false, // I am apparently not supposed to do this??
+		success: function(xml) {
+			$area = $(xml);
+			console.log($area.find("foo").text());
+		}
+	});
+}
+
 
 
 // turnTo is a macro to simplify the logic of cycling from view to view within a room, and
@@ -25,15 +44,15 @@ function turnTo(direction) {
 	} else if (direction == "left") {
 		viewindex--;
 		if (viewindex < 0) {
-			viewindex = kitchenviews.length - 1;
+			viewindex = 3;
 		}
 	} else if (direction == "right") {
 		viewindex++;
-		if (viewindex >= kitchenviews.length) {
+		if (viewindex >= $area.find("view").length - 1) {
 			viewindex = 0;
 		}
 	}
-	
+
 	navToIndex(viewindex);
 }
 
@@ -43,6 +62,7 @@ function turnTo(direction) {
 function navToIndex(index) {
 	if (index == undefined) {
 		index = 0;
-	}	
-	$(".game").css("background-image" , "url(\"img/" + kitchenviews[index] + ".png\")");
+	}
+	$(".game").css("background-image", "url(\"img/kitchen-" + $area.find("view").eq(index).text() + ".png\")");
+	//$(".game").css("background-image" , "url(\"img/" + kitchenviews[index] + ".png\")");
 }
